04edfabf2ce9efff0689fcf6cd04713e7fe4f143
glitchnobody <58382686+glitchnobody@users.noreply.github.com>
2023-04-05 15:36:11 +0530

    secon with api

--- src/pages/api/search.js
+++ src/pages/api/search.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { headers } from "../../../next.config";
 
 export default async function handler(req, res) {
   const { q } = req.query;
@@ -9,7 +10,11 @@ export default async function handler(req, res) {
     );
     const users = response.data.items;
     const userRequests = users.map((user) =>
-      axios.get(`https://api.github.com/users/${user.login}`)
+      axios.get(`https://api.github.com/users/${user.login}`, {
+        headers: {
+          Authorization: `Bearer ${process.env.GITHUB_KEY}`,
+        },
+      })
     );
     const userData = await Promise.all(userRequests);
     const userProfiles = userData.map((response) => response.data);
